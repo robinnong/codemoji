a4441a394a2c59afb4577262d5d03c6cc9c815f2
robinnong <robin.nong@hotmail.com>
2020-07-02 18:23:46 -0400

    Wrote basic structure for room code submission"

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 const Home = () => {
+    const [userInput, setInput] = useState("");
+    const [hasCode, setCodeSubmission] = useState(false); 
+
+    // Validate the user's input and bring them to the correct game room
+    const validateCode = (e) => {
+        e.preventDefault();
+        if (userInput === "") {
+            console.log("Invalid submission")
+        } else {
+            console.log(userInput)
+        }
+    }
+
+    // Sets the input field state on each input change
+    const handleUserInput = (e) => setInput(e.target.value.trim()); 
+
     return (
-        <div className="home">
-            <Link className="gameLink" to="/game">Start New Game</Link>
-            <button>or enter room code</button>
+        <div className="home"> 
+            {hasCode ? 
+            <form className="codeSubmit" onSubmit={validateCode}>
+                    <input type="text" onChange={handleUserInput} value={userInput} autoFocus placeholder="Enter code"/>
+                <button type="submit">Submit</button>
+            </form>
+            : 
+            <>
+                <Link className="gameLink" to="/game">Start New Game</Link>
+                <button onClick={() => setCodeSubmission(true)}>or enter room code</button>
+            </>
+            }
         </div>
     )
 }
